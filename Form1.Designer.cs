46594417837e85c882e3a83b0d7948e255be04bf
Sai <118180477+ESaiRoong@users.noreply.github.com>
2024-08-18 18:51:33 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace MenuShip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem7 = new ToolStripMenuItem();
+            toolStripMenuItem8 = new ToolStripMenuItem();
+            toolStripMenuItem5 = new ToolStripMenuItem();
+            toolStripMenuItem6 = new ToolStripMenuItem();
+            exitToolStripMenuItem1 = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            toolsToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            imageList1 = new ImageList(components);
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            txtMSSV = new TextBox();
+            txtHoTen = new TextBox();
+            button2 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, exitToolStripMenuItem, toolsToolStripMenuItem, viewToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem2, toolStripMenuItem3, toolStripMenuItem4, toolStripMenuItem5, toolStripMenuItem6, exitToolStripMenuItem1 });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.ShortcutKeys = Keys.Control | Keys.D1;
+            toolStripMenuItem2.Size = new Size(224, 26);
+            toolStripMenuItem2.Text = "1";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.CheckOnClick = true;
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(224, 26);
+            toolStripMenuItem3.Text = "2";
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem7, toolStripMenuItem8 });
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(224, 26);
+            toolStripMenuItem4.Text = "3";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(225, 26);
+            toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // toolStripMenuItem7
+            // 
+            toolStripMenuItem7.Name = "toolStripMenuItem7";
+            toolStripMenuItem7.Size = new Size(225, 26);
+            toolStripMenuItem7.Text = "toolStripMenuItem7";
+            // 
+            // toolStripMenuItem8
+            // 
+            toolStripMenuItem8.Name = "toolStripMenuItem8";
+            toolStripMenuItem8.Size = new Size(225, 26);
+            toolStripMenuItem8.Text = "toolStripMenuItem8";
+            // 
+            // toolStripMenuItem5
+            // 
+            toolStripMenuItem5.Name = "toolStripMenuItem5";
+            toolStripMenuItem5.Size = new Size(224, 26);
+            toolStripMenuItem5.Text = "4";
+            // 
+            // toolStripMenuItem6
+            // 
+            toolStripMenuItem6.Name = "toolStripMenuItem6";
+            toolStripMenuItem6.Size = new Size(224, 26);
+            toolStripMenuItem6.Text = "5";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            exitToolStripMenuItem1.ShortcutKeys = Keys.Control | Keys.E;
+            exitToolStripMenuItem1.Size = new Size(224, 26);
+            exitToolStripMenuItem1.Text = "Exit";
+            exitToolStripMenuItem1.Click += exitToolStripMenuItem1_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(49, 24);
+            exitToolStripMenuItem.Text = "Edit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            toolsToolStripMenuItem.Size = new Size(58, 24);
+            toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(55, 24);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(570, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "nhập ảnh";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnNhapAnh_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(538, 61);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(151, 149);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(txtMSSV);
+            groupBox1.Controls.Add(txtHoTen);
+            groupBox1.Location = new Point(12, 49);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 177);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin sinh viên";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(73, 150);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(51, 24);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "Nữ";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBoxNu_CheckedChanged;
+            checkBox2.CheckStateChanged += checkBoxNu_CheckedChanged;
+            checkBox2.Click += checkBoxNu_CheckedChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(75, 116);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(63, 24);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Nam";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBoxNam_CheckedChanged;
+            checkBox1.CheckStateChanged += checkBoxNam_CheckedChanged;
+            checkBox1.Click += checkBoxNam_CheckedChanged;
+            // 
+            // txtMSSV
+            // 
+            txtMSSV.Location = new Point(18, 71);
+            txtMSSV.Name = "txtMSSV";
+            txtMSSV.Size = new Size(182, 27);
+            txtMSSV.TabIndex = 1;
+            txtMSSV.Text = "MSSV";
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(18, 26);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(182, 27);
+            txtHoTen.TabIndex = 0;
+            txtHoTen.Text = "Họ và Tên";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(351, 286);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Xác nhận";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btnXacNhan_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private ToolStripMenuItem toolStripMenuItem5;
+        private ToolStripMenuItem toolStripMenuItem6;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem7;
+        private ToolStripMenuItem toolStripMenuItem8;
+        private ImageList imageList1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private TextBox txtMSSV;
+        private TextBox txtHoTen;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows.Forms;
+
+namespace MenuShip
+{
+    public partial class Form1 : Form
+    {
+        private List<SinhVien> danhSachSinhVien = new List<SinhVien>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void checkBoxNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void checkBoxNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+        private void btnNhapAnh_Click(object sender, EventArgs e)
+        {
+            // Tạo OpenFileDialog để chọn ảnh
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Chỉ cho phép chọn file ảnh (các định dạng phổ biến như .jpg, .jpeg, .png, .bmp, .gif)
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            // Hiển thị hộp thoại chọn file và kiểm tra xem người dùng có chọn file hay không
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Hiển thị ảnh đã chọn trong PictureBox
+                pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+
+                // Để ảnh hiển thị vừa với kích thước của PictureBox
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+        private void btnXacNhan_Click(object sender, EventArgs e)
+        {
+
+            string hoTen = txtHoTen.Text;
+            string mssv = txtMSSV.Text;
+            Image anh = pictureBox1.Image;
+
+
+            if (string.IsNullOrWhiteSpace(hoTen) || string.IsNullOrWhiteSpace(mssv) || anh == null)
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin và chọn ảnh.");
+                return;
+            }
+
+            // Tạo  SinhVien mới
+            SinhVien sinhVienMoi = new SinhVien
+            {
+                HoTen = hoTen,
+                MSSV = mssv,
+                Anh = anh
+            };
+
+            // Thêm sv vào danh sách
+            danhSachSinhVien.Add(sinhVienMoi);
+
+
+            MessageBox.Show("Đã thêm sinh viên vào danh sách!");
+
+            //  chuẩn bị cho lần nhập tiếp theo
+            txtHoTen.Clear();
+            txtMSSV.Clear();
+            pictureBox1.Image = null;
+        }
+
+
+
+    }
+    public class SinhVien
+    {
+        public string HoTen { get; set; }
+        public string MSSV { get; set; }
+        public Image Anh { get; set; }
+    }
+}
